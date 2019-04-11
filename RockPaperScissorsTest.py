0a0c90838a97f787879a21cfe165e0229d432b92
Bryce Harty <35123020+bhartyrs@users.noreply.github.com>
2019-04-11 15:25:00 -0400

    Update RockPaperScissorsTest.py
    Wrote 9 tests to test every input that we could possibly have. Used the game logic functions from RockPaperScissors.py
    
    All cases passed and it works well.

--- RockPaperScissorsTest.py
+++ RockPaperScissorsTest.py
@@ -5,20 +5,43 @@
 #importing the modules needed
 import unittest
 import RockPaperScissors
+import tkinter
 
 #creating a class to test RockPaperScissors.py
 class TestRockPaperScissors(unittest.TestCase):
 
 #This function will test compareResults to see if the expected outcomes are correct.
+ 	#########IMPORTANT###########
+		#When you run this, the dots before the test represent each test
+		#If it is a "." that case passed
+		#If it is a "F" that case failed
+		#It also give the number of cases and if all cases pass it will give an "OK"
+		#If a case fails or all of them it will say "Failure" and give which cases that were wrong. 
  	
-	def test_compareResults(self):		
-#		playerTwo_rock = playerTwo(0)
-#		playerTwo_paper = playerTwo(1)
-#		playerTwo_scissors = playerTwo(2)
-		playerTwo = ['rock', 'paper', 'scissors']
-		p2_rock = playerTwo[0]
+ 	#This are all three tests with Rock being playerOnesChoice
+	def test_Rock_Paper(self):		
+		self.assertEqual(RockPaperScissors.compareResultsConsole('rock', 'paper'), 2)
+	def test_Rock_Scissors(self):
+		self.assertEqual(RockPaperScissors.compareResultsConsole('rock', 'scissors'), 1)
+	def test_Rock_Rock(self):
+		self.assertEqual(RockPaperScissors.compareResultsConsole('rock', 'rock'), 0)
+
+
+ 	#This are all three tests with Paper being playerOnesChoice
+	def test_Paper_Rock(self):		
+		self.assertEqual(RockPaperScissors.compareResultsConsole('paper', 'rock'), 1)
+	def test_Paper_Scissors(self):		
+		self.assertEqual(RockPaperScissors.compareResultsConsole('paper', 'scissors'), 2)
+	def test_Paper_paper(self):		
+		self.assertEqual(RockPaperScissors.compareResultsConsole('paper', 'paper'), 0)
 		
-		self.assertEqual(RockPaperScissors.compareResults('rock'), p2_rock)
+ 	#This are all three tests with scissors being playerOnesChoice		
+	def test_Scissors_Scissors(self):		
+		self.assertEqual(RockPaperScissors.compareResultsConsole('scissors', 'scissors'), 0)
+	def test_Scissors_Rock(self):		
+		self.assertEqual(RockPaperScissors.compareResultsConsole('scissors', 'rock'), 2)
+	def test_Scissors_Paper(self):		
+		self.assertEqual(RockPaperScissors.compareResultsConsole('scissors', 'paper'), 1)
 
 
 if __name__ == '__main__':
